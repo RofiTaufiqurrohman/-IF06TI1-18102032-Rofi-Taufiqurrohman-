198846b93dbf7f89bbac21b24e926f323591c0c8
RofiTaufiqurrohman <72638135+RofiTaufiqurrohman@users.noreply.github.com>
2020-12-19 16:43:15 +0700

    create SettingPreferenceActivity

---
+++ praktikum9/src/main/java/com/rofi_18102032/praktikum9/SettingPreferenceActivity.kt
@@ -0,0 +1,11 @@
+package com.rofi_18102032.praktikum9
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SettingPreferenceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting_preference)
+    }
+}
