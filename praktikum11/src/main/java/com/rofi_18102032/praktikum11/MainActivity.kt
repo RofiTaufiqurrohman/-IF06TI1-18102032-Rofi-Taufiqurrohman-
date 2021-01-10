cb23ebe70e1ee6fdc1cc343a438f830935c7f728
RofiTaufiqurrohman <72638135+RofiTaufiqurrohman@users.noreply.github.com>
2021-01-10 09:19:06 +0700

    Init Practice 11

---
+++ praktikum11/src/main/java/com/rofi_18102032/praktikum11/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.rofi_18102032.praktikum11
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
