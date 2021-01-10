cb1966cec4f9fcf9b28624b8dc5e37b8a5feb14d
RofiTaufiqurrohman <72638135+RofiTaufiqurrohman@users.noreply.github.com>
2021-01-10 16:10:08 +0700

    menambahkan SignInActivity.kt

---
+++ praktikum11/src/main/java/com/rofi_18102032/praktikum11/SignInActivity.kt
@@ -0,0 +1,11 @@
+package com.rofi_18102032.praktikum11
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
